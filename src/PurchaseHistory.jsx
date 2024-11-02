0d2e97f5d107d5fc5292ae757bc429477d04a69a
siralusruthi123 <sruthisiralu0305@gmail.com>
2024-11-02 12:23:16 +0530

    Update PurchaseHistory.jsx

--- src/PurchaseHistory.jsx
+++ src/PurchaseHistory.jsx
@@ -4,6 +4,7 @@ function PurchaseHistory()
         <>
         <h1>This is PurchaseHistory</h1>
         <h1> this is modification PurchaseHistory</h1>
+            <h1> this is sruthi</h1>
         </>
     )
 }
